c0a632cc38731a300c7235caf183f8216644d853
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2011-02-21 09:29:14 +0100

    Se mejora la clase Template

--- classes/template.php
+++ classes/template.php
@@ -8,8 +8,8 @@
  */ 
   
 class Template { 
-  protected $name; // Template name
-  protected $data = array(); // Local and global data.
+  private $tName; // Template name
+  private $data = array(); // Local and global data.
   
   /** 
    * Constructor sets the template name, and makes sure 
@@ -21,20 +21,41 @@ class Template {
     if(!is_file(TEMPLATE_PATH.$name.TEMPLATE_EXT)) 
       die('Invalid template: '.$name); 
     
-    $this->name = $name; 
+    $this->tName = $name; 
   }
   
+  public function __get($name) {
+    if (array_key_exists($name, $this->data)) {
+      return $this->data[$name];
+    }
+    
+    $trace = debug_backtrace();
+    trigger_error(
+            'Undefined property via __get(): ' . $name .
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
+            E_USER_NOTICE);
+    return null;
+  }
+
   /** 
    * Set some template data. 
    * 
-   * @param key (string): Key of the data 
+   * @param name (string): Key of the data 
    * @param value (string): Value of the data 
    */ 
-  public function __set($key, $value) 
-  {
-    $this->data[$key] = $value; 
+  public function __set($name, $value) {
+    $this->data[$name] = $value; 
   }
   
+  public function __isset($name) {
+    return isset($this->data[$name]);
+  }
+
+  public function __unset($name) {
+    unset($this->data[$name]);
+  }
+
   /** 
    * Render a template.
    * 
@@ -44,7 +65,7 @@ class Template {
   { 
     return $this->render();
   } 
-     
+  
   /** 
    * Renders a template. 
    * 
@@ -57,7 +78,7 @@ class Template {
     // Extract data to local namespace. Don't worry extract isn't great 
     // but this is only in local scope so nothing to worry about :) 
     extract($this->data, EXTR_SKIP); 
-    require_once(TEMPLATE_PATH.$this->name.TEMPLATE_EXT);
+    require_once(TEMPLATE_PATH.$this->tName.TEMPLATE_EXT);
     $output = ob_get_clean();
     
     if($print) { 
