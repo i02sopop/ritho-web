7518713988efdb63521dc661a8642fbaa346cab9
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2011-03-08 02:12:02 +0100

    Check if the path exists

--- classes/view/v404.php
+++ classes/view/v404.php
@@ -65,7 +65,8 @@ class V404 extends View {
     $index->head->modernizr = '/js/modernizr-1.7.min.js';
     $index->head->gsVerification = 'Hr_OWj4SMe2RICyrXgKkj-rsIe-UqF15qtVk579MITk';
 
-    $index->path = $this->data['path'];
+    if(isset($this->path))
+      $index->path = $this->path;
 
     $index->render(true);
   }
