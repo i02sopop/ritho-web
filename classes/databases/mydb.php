13090a57ea8fd543d6cf3504b4d514d60dd3c96a
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2012-03-13 00:19:46 +0100

    Little formatting enhacements and filled some mysql methods.

--- classes/databases/mydb.php
+++ classes/databases/mydb.php
@@ -1,27 +1,26 @@
 <?php
-/*Copyright (c) 2011-2012 Ritho-web team (look at AUTHORS file)
+/* Copyright (c) 2011-2012 Ritho-web team (look at AUTHORS file)
   
-  This file is part of ritho-web.
+   This file is part of ritho-web.
 
-  ritho-web is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Affero General Public License as
-  published by the Free Software Foundation, either version 3 of the
-  License, or (at your option) any later version.
+   ritho-web is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
 
-  ritho-web is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Affero General Public License for more details.
+   ritho-web is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
 
-  You should have received a copy of the GNU Affero General Public
-  License along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU Affero General Public
+   License along with Foobar. If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*
-  Basic database engine.
+/* Basic database engine.
 
-  @author Ritho-web team
-  @copyright Copyright (c) 2011-2012 Ritho-web team (look at AUTHORS file)
+   @author Ritho-web team
+   @copyright Copyright (c) 2011-2012 Ritho-web team (look at AUTHORS file)
 */
 class MyDB extends DB {
     /* Constructor of the class.
@@ -53,19 +52,21 @@ class MyDB extends DB {
        @return Connection resource on success, FALSE on failure.
     */
     public function connect() {
-        return mysql_connect('example.com:3307',  'mysql_user', 'mysql_password');
+        return mysql_connect($host . ':' . $port,  $user, $pass);
     }
 
     /* Delete records from a table specified by the keys and values in assoc_array.
 
        @param $table_name (string): Name of the table from which to delete rows.
-       @param $assoc (array): An array whose keys are field names in the table table_name, and whose values are the values of those fields that are to be deleted.
+       @param $assoc (array): An array whose keys are field names in the table
+              table_name, and whose values are the values of those fields that are
+              to be deleted.
        @param $conn (resource): Database connection resource.
        @return TRUE on success, FALSE on failure.
     */
     public function delete($table_name, $assoc, $conn = NULL) {
-        $query = "DELETE FROM " . $table_name . " WHERE ";
-        mysql_query($query);
+        $query = "DELETE FROM " . $table_name;
+        mysql_query($query, $conn);
     }
 
     /* Escape a string for insertion into the database.
@@ -85,10 +86,13 @@ class MyDB extends DB {
     public function exec($query, $conn = NULL) {
     }
 
-    /* Sends a request to execute a prepared statement with given parameters, without waiting for the result(s).
+    /* Sends a request to execute a prepared statement with given parameters,
+       without waiting for the result(s).
 
        @param $stmtname (string): The name of the prepared statement to execute.
-       @param $params (array): Array of parameter values to substitute for the placeholders in the original prepared query string. The number of elements in the array must match the number of placeholders.
+       @param $params (array): Array of parameter values to substitute for the
+              placeholders in the original prepared query string. The number of
+              elements in the array must match the number of placeholders.
        @param $conn (resource): Database connection resource.
        @return TRUE on success, FALSE on failure.
     */
@@ -98,7 +102,9 @@ class MyDB extends DB {
     /* Get an array that contains all rows (records) in the result resource.
 
        @param $result (resource): Query result resource.
-       @return Array with all rows in the result. Each row is an array of field values indexed by field name. FALSE if there are no rows in the result, or on any other error.
+       @return Array with all rows in the result. Each row is an array of field
+               values indexed by field name. FALSE if there are no rows in the
+               result, or on any other error.
     */
     public function fetch_all($result) {
     }
@@ -106,9 +112,13 @@ class MyDB extends DB {
     /* Fetch a row into a numbered array from a query result.
 
        @param $result (resource): Result to get the row.
-       @param $result_type (int): Parameter to control how the returned array is indexed. result_type is a constant and can take the following values: SQL_ASSOC, SQL_NUM and SQL_BOTH.
+       @param $result_type (int): Parameter to control how the returned array is
+              indexed. result_type is a constant and can take the following values:
+              SQL_ASSOC, SQL_NUM and SQL_BOTH.
        @param $row (int): Row to fetch.
-       @return Array indexed numerically, associatively or both, FALSE on error. Each value in the array is represented as a string. Database NULL values are returned as NULL.
+       @return Array indexed numerically, associatively or both, FALSE on error.
+               Each value in the array is represented as a string. Database NULL
+               values are returned as NULL.
     */
     public function fetch_array($result, $result_type = 0, $row = -1) {
     }
@@ -117,12 +127,15 @@ class MyDB extends DB {
 
        @param $result (resource): Result to get the row.
        @param $row (int): Row to fetch.
-       @return Array indexed associatively, FALSE on error. Each value in the array is represented as a string. Database NULL values are returned as NULL.
+       @return Array indexed associatively, FALSE on error. Each value in the array
+               is represented as a string. Database NULL values are returned as NULL.
     */
     public function fetch_assoc($result, $row = -1) {
     }
 
-    /* Fetch an object with properties that correspond to the fetched row's field names. It can optionally instantiate an object of a specific class, and pass parameters to that class's constructor.
+    /* Fetch an object with properties that correspond to the fetched row's field
+       names. It can optionally instantiate an object of a specific class, and pass
+       parameters to that class's constructor.
 
        @param $result (resource): Result to get the row.
        @param $row (int): Row to fetch.
@@ -138,12 +151,14 @@ class MyDB extends DB {
 
        @param $result (resource): Result to get the row.
        @param $row (int): Row to fetch.
-       @return Array indexed numerically, FALSE on error. Each value in the array is represented as a string. Database NULL values are returned as NULL.
+       @return Array indexed numerically, FALSE on error. Each value in the array is
+               represented as a string. Database NULL values are returned as NULL.
     */
     public function fetch_row($result, $row = -1) {
     }
 
-    /* Get the name of the field occupying the given field_number in the given result resource. Field numbering starts from 0.
+    /* Get the name of the field occupying the given field_number in the given
+       result resource. Field numbering starts from 0.
 
        @param $result (resource): Result to get the name of the column.
        @param $field_number (integer): Number of field to check.
@@ -152,7 +167,8 @@ class MyDB extends DB {
     public function field_name($result, $field_number = -1) {
     }
 
-    /* Get a string containing the base type name of the given field_number in the given result resource.
+    /* Get a string containing the base type name of the given field_number in the
+       given result resource.
 
        @param $result (resource): Result resource to get the type of the field.
        @param $field_number (int): Number of field to check.
@@ -173,7 +189,9 @@ class MyDB extends DB {
 
        @param $connection (resource): Database connection resource.
        @param $table_name (string): Name of the table into which to insert rows.
-       @param $assoc (array): Array whose keys are field names in the table table_name, and whose values are the values of those fields that are to be inserted.
+       @param $assoc (array): Array whose keys are field names in the table
+              table_name, and whose values are the values of those fields that are
+              to be inserted.
        @return TRUE on success, FALSE on failure.
     */
     public function insert($connection, $table_name, $assoc) {
@@ -211,8 +229,11 @@ class MyDB extends DB {
 
     /* Creates a prepared statement for later execution.
 
-       @param $stmtname (string): The name to give the prepared statement. Must be unique per-connection.
-       @param $query (string): The parameterized SQL statement. Must contain only a single statement. If any parameters are used, they are referred to as $1, $2, etc.
+       @param $stmtname (string): The name to give the prepared statement. Must be
+              unique per-connection.
+       @param $query (string): The parameterized SQL statement. Must contain only a
+              single statement. If any parameters are used, they are referred to as
+              $1, $2, etc.
        @param $conn (resource): Database connection resource.
        @return TRUE on success, FALSE on failure.
     */
@@ -231,7 +252,9 @@ class MyDB extends DB {
     /* Query a prepared statement.
 
        @param $stmtname (string): The name of the prepared statement to execute.
-       @param $params (array): Array of parameter values to substitute for the placeholders in the original prepared query string. The number of elements in the array must match the number of placeholders.
+       @param $params (array): Array of parameter values to substitute for the
+              placeholders in the original prepared query string. The number of
+              elements in the array must match the number of placeholders.
        @param $conn (resource): Database connection resource.
        @return Values that match the query.
     */
@@ -241,7 +264,9 @@ class MyDB extends DB {
     /* Select records specified by assoc_array which has field=>value.
 
        @param $table_name (string): Name of the table from which to select rows.
-       @param $assoc (array): Array whose keys are field names in the table table_name, and whose values are the conditions that a row must meet to be retrieved.
+       @param $assoc (array): Array whose keys are field names in the table
+              table_name, and whose values are the conditions that a row must meet
+              to be retrieved.
        @param $conn (resource): Database connection resource.
        @return Query result resource on success, FALSE on failure.
     */
