99cb980e1eb039adc74633a6c8d7416524898c35
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2015-11-15 22:30:49 -0600

    Considering only the path to run the proper controller.

--- www/index.php
+++ www/index.php
@@ -1,10 +1,10 @@
 <?php
-/* Copyright (c) 2011-2012 Ritho-web team (look at AUTHORS file)
+/* Copyright (c) 2011-2015 Ritho-web team (look at AUTHORS file)
 
    This file is part of ritho-web.
 
    ritho-web is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License as 
+   it under the terms of the GNU Affero General Public License as
    published by the Free Software Foundation, either version 3 of the
    License, or (at your option) any later version.
 
@@ -17,19 +17,38 @@
    License along with Foobar. If not, see <http://www.gnu.org/licenses/>.
 */
 
-require_once('configuration.php');
+/** File index.php.
+ *
+ * Landing page for the users.
+ *
+ * @category  Index
+ * @version   GIT: <git_id>
+ * @link      http://ritho.net
+ */
 
-// TODO: This wil go to the main class.
-$controller = null; /* Controller to run. */
-$requested = empty($_SERVER['REQUEST_URI']) ? false : $_SERVER['REQUEST_URI']; /* URI requested. */
-switch ($requested) {
+require_once 'configuration.php';
+
+/** TODO: This wil go to the main class. */
+/** Controller to run. */
+$controller = null;
+
+/** URI requested. */
+$request = empty($_SERVER['REQUEST_URI']) ?
+    false :
+    $_SERVER['REQUEST_URI'];
+
+$path = (!empty($_SERVER['QUERY_STRING'])) ?
+	substr($requested, 0, strpos($requested, $_SERVER['QUERY_STRING']) -1) :
+	$requested;
+
+switch ($path) {
 case '/':
 case '/index':
 case '/index.php':
     $controller = new CIndex();
     break;
 default:
-    $controller = new C404(substr($requested, 1));
+    $controller = new C404($path);
     break;
 }
 
